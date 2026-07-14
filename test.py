1d8e9b4d76d9a6ff65f5e4d06296b5af5c53ec64
Khaled4Emad <khaled_emad@watanya.com>
2026-07-14 23:01:49 +0300

    pylint liberary

---
+++ test.py
@@ -0,0 +1,14 @@
+'''
+This is my module
+to test pylint liberary
+to test in command Pallete write 
+pylint.exe + Your File Path 
+or
+python -m pylint + Your File Path
+'''
+
+def say_hello(name):
+    '''This Function is to say Hello'''
+    return f"Hello {name}"
+
+say_hello("Khaled")
